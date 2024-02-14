a542c302159b1ed150423957759ea823a2a7edd7
Borges <sdafdsf@.asdsa>
2024-02-14 10:39:31 -0300

    adding learning cubit state management

--- learning_cubit/lib/second_page.dart
+++ learning_cubit/lib/second_page.dart
@@ -1,7 +1,5 @@
 import 'dart:math';
 
-import 'package:bloc/bloc.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
