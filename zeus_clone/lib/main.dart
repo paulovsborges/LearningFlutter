84b3ea3953be8560b0ed29ab3f0fac276754ee16
Borges <sdafdsf@.asdsa>
2024-02-14 15:19:12 -0300

    adding zeus clone

--- zeus_clone/lib/main.dart
+++ zeus_clone/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,95 +33,106 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: BlocProvider(
+          create: (_) => PageControllerState(),
+          child: const LandingPage(),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var navigationIndexWatcher =
+        context.watch<PageControllerState>().state.currentPageIndex;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: PageView(
+        controller: context.read<PageControllerState>().state.pageController,
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.amber,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (newPageIndex) {
+          context
+              .read<PageControllerState>()
+              .changeBottomNavigationPageIndex(newPageIndex);
+        },
+        currentIndex: navigationIndexWatcher,
+        type: BottomNavigationBarType.fixed,
+        selectedIconTheme: const IconThemeData(color: Colors.black, size: 24),
+        unselectedIconTheme:
+            const IconThemeData(color: Colors.black12, size: 16),
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/images/categories.svg'),
+              label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add, color: Colors.black), label: 'Passwords'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add, color: Colors.black), label: 'item 3'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add, color: Colors.black), label: 'item 4'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add, color: Colors.black), label: 'item 5'),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class PageControllerState extends Cubit<LandingPageState> {
+  PageControllerState()
+      : super(LandingPageState(
+            pageController: PageController(), currentPageIndex: 0));
+
+  void changeBottomNavigationPageIndex(int newIndex) {
+    const animDuration = Duration(milliseconds: 200);
+    var animCurve = Curves.bounceIn;
+
+    state.pageController
+        .animateToPage(newIndex, duration: animDuration, curve: animCurve);
+
+    emit(
+      LandingPageState(
+          pageController: state.pageController, currentPageIndex: newIndex),
+    );
+  }
+}
+
+class LandingPageState {
+  LandingPageState(
+      {required this.pageController, required this.currentPageIndex});
+
+  final PageController pageController;
+  final int currentPageIndex;
 }
