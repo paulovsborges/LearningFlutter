aac3a152375d13853eb4c18f269b0a98edea3092
Borges <sdafdsf@.asdsa>
2024-02-17 20:02:59 -0300

    feat: add custom fonts

--- locktap/lib/screens/onboarding/on_boarding.dart
+++ locktap/lib/screens/onboarding/on_boarding.dart
@@ -4,7 +4,8 @@ import 'package:locktap/screens/onboarding/page_indicator.dart';
 import 'package:locktap/screens/onboarding/screenshots_grid.dart';
 import 'package:locktap/util/app_colors.dart';
 
-class OnBoardingScreen extends StatelessWidget with ScreenshotsGrid, PageIndicator {
+class OnBoardingScreen extends StatelessWidget
+    with ScreenshotsGrid, PageIndicator {
   const OnBoardingScreen({super.key});
 
   Widget _buildBottomSheetView() {
@@ -17,6 +18,31 @@ class OnBoardingScreen extends StatelessWidget with ScreenshotsGrid, PageIndicat
             height: 10,
           ),
           buildPageIndicator(),
+          const SizedBox(
+            height: 42,
+          ),
+          const Text(
+            'Universal data storage',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sf',
+              fontWeight: FontWeight.w600,
+              fontSize: 24,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text(
+              'Relieve yourself of creating and \nmemorizing complex passwords on your own with Locktap',
+              style: TextStyle(
+                color: AppColors.gray,
+                fontFamily: 'sf',
+                fontWeight: FontWeight.w100,
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ],
       ),
     );
@@ -24,26 +50,28 @@ class OnBoardingScreen extends StatelessWidget with ScreenshotsGrid, PageIndicat
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          color: AppColors.backgroundColor,
-          child: buildScreenshotsFrid(
-            [
-              'assets/images/onboarding/first_step/img_onboarding_1_1.png',
-              'assets/images/onboarding/first_step/img_onboarding_1_2.png',
-              'assets/images/onboarding/first_step/img_onboarding_1_3.png',
-              'assets/images/onboarding/first_step/img_onboarding_1_4.png',
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: AppColors.backgroundColor,
+            child: buildScreenshotsFrid(
+              [
+                'assets/images/onboarding/first_step/img_onboarding_1_1.png',
+                'assets/images/onboarding/first_step/img_onboarding_1_2.png',
+                'assets/images/onboarding/first_step/img_onboarding_1_3.png',
+                'assets/images/onboarding/first_step/img_onboarding_1_4.png',
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              _buildBottomSheetView(),
             ],
           ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            _buildBottomSheetView(),
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- locktap/lib/util/app_colors.dart
+++ locktap/lib/util/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const backgroundColor = Color.fromARGB(255, 16, 23, 39);
   static const secondaryBackground = Color.fromARGB(255, 35, 41, 56);
   static const lightBlue = Color.fromARGB(255, 0, 184, 238);
+  static const gray = Color.fromARGB(255, 153, 153, 153);
 }
